e9f0d540c567bd44dc2031e673a54ccef957d0db
austinsk8s <austinsk8s2day@gmail.com>
2014-03-22 15:27:46 -0500

    Added a JavaScript and CSS directory and moved files appropriately for organizational purposes

---
+++ GameBoy/js/CPU.js
@@ -0,0 +1,20 @@
+//These are just the registers, timers and other variables that is needed in z80
+
+//CPU Registers
+var rA=0; 	// Accul
+var rB=0; 	// register B
+var rC=0; 	// register C
+var rD=0; 	// register D
+var rE=0; 	// register E
+
+var HL=0; 	// Both registers H and L
+var SP=0;		// Stack Pointer
+var PC=0; 	// Program Counter
+
+var ZF=0,	// Zero Flag
+	HF=0,	// Half Flag
+	SF=0,	// sub Flag
+	CF=0;	// Carry flag
+
+var T1=0; 	// temp registers
+var T2=0;

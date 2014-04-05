cd4d7d0687c72025dd2c035004cb4bb4848143e8
jfkilpat <kilpatrick.jon@gmail.com>
2014-04-05 11:35:02 -0500

    Added the tile update checker function
    Allows the update background function to know what needs to b e updated

--- GameBoy/js/LCD.js
+++ GameBoy/js/LCD.js
@@ -23,6 +23,32 @@ var Colors    = [[0xEF,0xFF,0xDE],[0xAD,0xD7,0x94],
                    [0x52,0x92,0x73],[0x18,0x34,0x42]];
 
 
+function Update_Tile_Data() {
+	var tda = 0;	//tile data address
+	var line = 0;	//2 byte line
+	var j = 0;
+	//Loop tiles and see if redraw is needed
+	for (var i=0;i<384;i++) if (UDtilesList[i]) {
+		tda=0x8000+i*16;
+			for (j=0; j<8; j++) { //Loop 8 lines
+				line = Memory[tda++];
+				line|= Memory[tda++] << 8;
+				TileData[i][j][0] = ((line & 0x8080) + 0x3FFF) >> 14;
+				TileData[i][j][1] = ((line & 0x4040) + 0x1FFF) >> 13;
+				TileData[i][j][2] = ((line & 0x2020) + 0x0FFF) >> 12;
+				TileData[i][j][3] = ((line & 0x1010) + 0x07FF) >> 11;
+				TileData[i][j][4] = ((line & 0x0808) + 0x03FF) >> 10;
+				TileData[i][j][5] = ((line & 0x0404) + 0x01FF) >> 9;
+				TileData[i][j][6] = ((line & 0x0202) + 0x00FF) >> 8;
+				TileData[i][j][7] = ((line & 0x0101) + 0x007F) >> 7; 
+			}
+		// marking these tiles for update in Update_Background()
+		UDbgDataList[i] = UDbg = true;
+		UDtilesList[i] = false;
+	}
+	UDtiles = false;
+}
+  
 function Canvas() {
 
 var Output = "CPU regs, SP, and PC values";

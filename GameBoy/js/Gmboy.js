4a4774a9be663ac4f9e17330e49fc6d81deee966
austinsk8s <austinsk8s2day@gmail.com>
2014-04-05 19:33:54 -0500

    Added a rom to click on and test (error)

--- GameBoy/js/Gmboy.js
+++ GameBoy/js/Gmboy.js
@@ -8,18 +8,17 @@ function GBPause() {
   clearInterval(FpsInterval);
 }
 
-/*function Insert_Cartridge(fileName, Start) {
+
+function Insert_Cartridge(fileName, Start) {
   GBPause();
   Seconds = 0;
   Frames  = 0;
-  //gb_Init_Debugger();
-  gb_Init_Memory();
-  gb_Init_LCD();
-  gb_Init_Interrupts();
-  gb_Init_CPU();
-  gb_Init_Input();
-  gb_ROM_Load('roms/'+fileName);
-  gb_Dump_All();
+//  Init_Memory();
+//  Init_LCD();
+//  Init_CPU();
+//  Init_Input();
+RunTest();
+  ROM_Load('roms/'+fileName);
   if (Start) $('BR').onclick();
   else $('BP').onclick();
-}*/
+}

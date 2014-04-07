c7b7292e8d1026b46c7cd401efd559c2ef8bd5a4
mjmcgill <pinkobob12@yahoo.com>
2014-04-07 01:08:39 -0500

    Update input.js

--- GameBoy/js/input.js
+++ GameBoy/js/input.js
@@ -5,19 +5,63 @@ var Pin15=0; // start select a b
 function OnKeyDown(e) {
     // start
 if (e.keyCode == 65) {
-    Pin15&=0xF7; MEMW(_IF_,RegIF|16); e.preventDefault();
-$('DEBUGGER').innerHTML=CPUdebugReg;
-testCPU();
+    Pin15&=0xF7; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+    $('DEBUGGER').innerHTML=CPUdebugReg;
+    testCPU();
 }
-//    return;
 
  // select
 if (e.keyCode == 83) {
-    Pin15&=0xFB; MEMW(_IF_,RegIF|16); e.preventDefault(); 
-$('OPCODES').innerHTML=CPUdebugOP;
-TestOPS();
+    Pin15&=0xFB;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault(); 
+    $('OPCODES').innerHTML=CPUdebugOP;
+    TestOPS();
 }
 
+// a
+ if (e.keyCode == 88) {
+    Pin15&=0xFE;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// b
+ if (e.keyCode == 90) {
+    Pin15&=0xFD;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// up
+ if (e.keyCode == 38) {
+    Pin14&=0xFB;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// down
+ if (e.keyCode == 40) {
+    Pin14&=0xF7; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+    
+    
+// left
+ if (e.keyCode == 37) {
+    Pin14&=0xFD; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// right
+ if (e.keyCode == 39) {
+    Pin14&=0xFE; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
 
 return;
 }
@@ -25,17 +69,62 @@ return;
 function OnKeyUp(e) {
     // start
 if (e.keyCode == 65) {
-    Pin15|=8; MEMW(_IF_,RegIF|16); e.preventDefault();
-$('DEBUGGER').innerHTML=CPUdebugReg;
-testCPU();
+    Pin15|=8;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+    $('DEBUGGER').innerHTML=CPUdebugReg;
+    testCPU();
 }
-//    return;
 
      // select
 if (e.keyCode == 83) {
-    Pin15&=0xFB; MEMW(_IF_,RegIF|16); e.preventDefault(); 
-$('OPCODES').innerHTML=CPUdebugOP;
-TestOPS();
+    Pin15&=4;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault(); 
+    $('OPCODES').innerHTML=CPUdebugOP;
+    TestOPS();
+}
+
+// a
+ if (e.keyCode == 88) {
+    Pin15&=1;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// b
+ if (e.keyCode == 90) {
+    Pin15&=2;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// up
+ if (e.keyCode == 38) {
+    Pin14&=4;
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// down
+ if (e.keyCode == 40) {
+    Pin14&=8; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+    
+    
+// left
+ if (e.keyCode == 37) {
+    Pin14&=2; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
+}
+
+// right
+ if (e.keyCode == 39) {
+    Pin14&=1; 
+    MEMW(_IF_,RegIF|16);
+    e.preventDefault();
 }
 
 

6966ccedf63a731ff3653e28f569282706ff6c86
austinsk8s <austinsk8s2day@gmail.com>
2014-04-19 00:55:57 -0500

    Added License

--- GameBoy/js/LCDtimers.js
+++ GameBoy/js/LCDtimers.js
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   Project Lambros Game Boy CPU.js					   *
+ *   Project Lambros Game Boy LCDtimers.js                         					   *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *

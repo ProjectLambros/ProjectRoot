c4443a1e12fd76160cde2b141e9ddb5b71921cfd
austinsk8s <austinsk8s2day@gmail.com>
2014-04-19 00:59:26 -0500

    Added License

--- GameBoy/js/input.js
+++ GameBoy/js/input.js
@@ -1,3 +1,22 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *   Project Lambros Game Boy input.js                                     *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   The full license is available at http://www.gnu.org/licenses/gpl.html *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+
+
 var Pin14=0; // up down left right
 var Pin15=0; // start select a b
 

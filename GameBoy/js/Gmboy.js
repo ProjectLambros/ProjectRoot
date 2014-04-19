046a7f3cf2a52b8955fb8706ca78ea293bb80ff5
austinsk8s <austinsk8s2day@gmail.com>
2014-04-19 00:57:27 -0500

    Added License

--- GameBoy/js/Gmboy.js
+++ GameBoy/js/Gmboy.js
@@ -1,5 +1,5 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   Project Lambros Game Boy Gmboy.js	                        				   *
+ *   Project Lambros Game Boy Gmboy.js                                     *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
